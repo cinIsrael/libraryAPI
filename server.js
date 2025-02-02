ccfdd3e9b23ee77e0c33848008f4de64b0d70e0e
cinIsrael <Israelabina05@gmail.com>
2025-02-02 18:22:09 +0100

    update

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
-const app = require("./src/app");
+const app =require("./src/app")
 const connectDB = require("./src/config/db");
 
 connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+const express = require('express');
+
+app.get('/', (req, res) => {
+  res.send('Server is running on Vercel!');
+});
+
+// Export the Express app as a Vercel function
+module.exports = app;
